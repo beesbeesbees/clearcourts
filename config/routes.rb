3c0413e716831638a152a71c7d0e52ad2cdef90d
reidelliott <reid@efeqdev.com>
2015-09-12 18:44:48 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
 
+  get :begin, to: "home#begin"
+  get :last_citation, to: "home#last_citation"
   get :dob, to: "home#dob"
+  get :citations, to: "home#citations"
+  get :single_citation, to: "home#single_citation"
+
+
+  get :sessions, to: "home#sessions"
+
 
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
