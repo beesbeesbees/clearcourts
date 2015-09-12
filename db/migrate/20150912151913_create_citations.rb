7f09639c45dc81404f716efb480ad1a7586c1fc4
Chris Zempel <zempel@efeqdev.com>
2015-09-12 10:20:37 -0500

    adds citations

---
+++ db/migrate/20150912151913_create_citations.rb
@@ -0,0 +1,19 @@
+class CreateCitations < ActiveRecord::Migration
+  def change
+    create_table :citations do |t|
+      t.integer :citation_number
+      t.datetime :citation_date
+      t.string :first_name
+      t.string :last_name
+      t.datetime :date_of_birth
+      t.string :defendant_address
+      t.string :defendant_city
+      t.string :defendent_state
+      t.string :drivers_license_number
+      t.integer :court_id
+      t.datetime :court_date
+
+      t.timestamps null: false
+    end
+  end
+end
