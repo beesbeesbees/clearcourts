03528497371c50562d3b7b9f771ef9d81173af3f
B <public88878878887@hotmail.com>
2015-09-12 22:42:54 +0000

    annotate

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: reminders
+#
+#  id         :integer          not null, primary key
+#  send_date  :datetime
+#  session_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Reminder < ActiveRecord::Base
   belongs_to :session
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: sessions
+#
+#  id             :integer          not null, primary key
+#  expiration     :datetime
+#  phone_number   :string(255)
+#  email          :string(255)
+#  contact_method :string(255)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  state          :string(255)
+#
+
 class Session < ActiveRecord::Base
   has_many :reminders
 end
