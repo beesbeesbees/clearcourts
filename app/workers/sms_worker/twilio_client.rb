9d1115ec60eb1c4a67dee99790ac1d564429f6cd
B <public88878878887@hotmail.com>
2015-09-12 21:01:08 +0000

    bugfix

--- app/workers/sms_worker/twilio_client.rb
+++ app/workers/sms_worker/twilio_client.rb
@@ -3,7 +3,7 @@ class SmsWorker::TwilioClient < Object
   def initialize
     account_sid= ENV['TWILIO_ACCOUNT_SID'.freeze]
     account_token= ENV['TWILIO_ACCOUNT_TOKEN'.freeze]
-    @account= Twilio::REST::Client.new(account_sid, account_token).account.account
+    @account= Twilio::REST::Client.new(account_sid, account_token).account
   end
 
   def send_sms to:, body:
@@ -12,7 +12,7 @@ class SmsWorker::TwilioClient < Object
         from: ENV['CLEARCOURTS_PHONE_NUMBER'.freeze],
         to: to,
         body: body,
-      )
+      ).!.!
     rescue Twilio::REST::RequestError => e
       Rails.logger.error e.message
       false
