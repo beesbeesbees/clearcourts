ff25d71afe6fcdbe8691dcf974bf090d03885ce0
B <public88878878887@hotmail.com>
2015-09-12 21:44:54 +0000

    sms route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
 
   root 'home#index'
+  post :sms
 
   namespace :admin do
     resources :users
