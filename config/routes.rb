413e5bea5cfd6bbf479c9270ba0d4d57a819c83f
B <public88878878887@hotmail.com>
2015-09-12 22:31:49 +0000

    fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
 
-  root 'home#index'
   post :sms, to: 'twilio#sms'
 
   namespace :admin do
@@ -22,5 +21,6 @@ Rails.application.routes.draw do
   authenticated :user do
     root to: "home#landing", as: :user_root
   end
+  root 'home#index' #non-users
 
 end
