01f62b70b0752914a25ea7885c301cc3bbb1c698
B <public88878878887@hotmail.com>
2015-09-13 08:02:18 +0000

    sms traffic

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -4,12 +4,15 @@ class SmsController < ApplicationController
   def greeting_1
 Rails.logger.debug "params== #{params}"
     @session= Session.where(phone_number: params[:From]).first_or_initialize
-    @session.update!(state: 'greeting_3'.freeze) #next stage
+    @session.update!(
+      phone_number: params['Body'.freeze],
+      state: 'greeting_3'.freeze, #next stage
+    )
     render partial: 'greeting_1'.freeze
   end
 
   def greeting_3
-    @session= Session.where(phone_number: params[:From]).first_or_initialize
+    @session= Session.where(phone_number: params[:From]).first
     @session.update!(state: 'greeting_1'.freeze) #next stage
     render partial: 'greeting_3'.freeze
   end
