78c5c57d13fe8ae527349e482114618e446b83c3
B <public88878878887@hotmail.com>
2015-09-13 06:51:34 +0000

    move to class method

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -4,7 +4,7 @@ class TwilioController < ApplicationController
 
   def sms
     @session= Session.where(phone_number: twilio_params[:From]).first_or_initialize
-    SmsWorker.new.perform_in(2.seconds,
+    SmsWorker.perform_in(2.seconds,
       twilio_params[:From],  #send the text to the number we received this one from
       render_action_to_s(SmsController, @session.next_action.to_sym, twilio_params) #render the view into the SMS body
     )
