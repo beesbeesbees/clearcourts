ce7deca51adbfeb2119bf450d4ed7115e16b7700
B <public88878878887@hotmail.com>
2015-09-13 06:35:27 +0000

    move render

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -26,4 +26,22 @@ private
       :AccountSid, :From, :ApiVersion
     )
   end
+
+  def render_action_to_s(controller, action, params)
+    controller.class_eval do
+      def params=(params)
+        @params= params
+      end
+      def params
+        @params
+      end
+    end
+    instance= controller.new
+    instance.request= @_request
+    instance.response= @_response
+    instance.params= params
+    instance.process_action(action)
+    instance.send(action)
+    instance.response.body
+  end
 end
