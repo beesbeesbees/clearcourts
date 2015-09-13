859e37ea24c606dd6fcfa932d9559fa04fcf41cc
Chris Zempel <zempel@efeqdev.com>
2015-09-13 00:57:24 -0500

    csv uploads working

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -1,6 +1,27 @@
+require 'csv'
+
 class Citation < ActiveRecord::Base
   has_many :violations
   belongs_to :court
 
   accepts_nested_attributes_for :violations
+
+
+  def self.import(file)
+    count = 0
+
+    csv_text = File.read(file.path)
+    csv = CSV.parse(csv_text, :headers => true)
+
+    csv.each do |row|
+      court = CreateCourt.call(
+        row['court_address'],
+        row['court_location']
+      )
+
+      citation = CreateCitation.call(row, court)
+      count += 1
+    end
+    count
+  end
 end
